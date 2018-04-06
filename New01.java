06a7ac2695d90e52e7b8e232567e396548d4fa39
Hayden Redd <haydenredd@Haydens-MacBook-Pro.local>
2018-04-06 10:10:05 -0500

    first commit. New01 is a basic JTextArea actionlistener implementation.Will be gui for Verify app.

---
+++ New01.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.event.*;
+//https://javaproglang.blogspot.com/2014/01/jtextarea-on-jpanel-implements.html#.WseGb2aZPow
+// for jpassworfield https://javaproglang.blogspot.com/2014/01/jtextfield-jpasswordfield-on-jpanel.html#.WseKn2aZPow
+
+public class New01 extends JPanel implements ActionListener
+{
+	JLabel lblName;
+	JTextField txtName;
+	JTextArea taRemarks;
+	JButton btnIcon;
+	//ImageIcon imgIcon;
+	
+	public New01()
+	{
+		setLayout(null);
+		lblName = new JLabel("Enter number: ");
+		txtName = new JTextField(15);
+		taRemarks = new JTextArea(10,10);
+		btnIcon = new JButton("Verify");
+		btnIcon.addActionListener(this);
+		
+		lblName.setBounds(10,20,120,25);
+		txtName.setBounds(150,20,150,25);
+		btnIcon.setBounds(120,100,150,30);
+		taRemarks.setBounds(30,140,330,100);
+		
+		//Add labels to the Panel.
+		add(lblName);
+		add(txtName);
+		add(btnIcon);
+		add(taRemarks);
+	}
+	public void actionPerformed(ActionEvent actEvt)
+	{
+		if (actEvt.getSource() == btnIcon)
+		{
+			taRemarks.append("Name: "+txtName.getText()+"\n");			
+		}
+	}
+
+
+
+	
+	public static void main(String[] args) {
+		New01 newgui = new New01();
+		
+		// set text on frame
+		JFrame frame = new JFrame("Text Area Demo");
+		frame.setContentPane(newgui);
+		
+		// setSize() methods is used to specify the width and height of the frame
+		frame.setSize(400,300);
+
+		// To display the Frame
+		frame.setVisible(true);
+		
+		WindowListener listener = new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent winEvt)
+			{
+				System.exit(0);
+			}
+		};  // End of WindowAdaptor() method
+
+		// Window listener activates the windowClosing() method
+		frame.addWindowListener(listener);
+	}
+		
+
+	}
+
+
