391d597317a7b2a2550be3d18c85fb4a76f0776b
Hayden Redd <haydenredd@Haydens-MacBook-Pro.local>
2018-04-07 11:03:01 -0500

    Test_RestAPIClient.java refactored, json attributes reached by setting access modifiers to class fields.

--- Test_RestAPIClient.java
+++ Test_RestAPIClient.java
@@ -8,29 +8,23 @@ import org.json.simple.*;
 
 
 public class Test_RestAPIClient {
-	public String number;
-	public static String carrier;
-	public JSONObject jsonObject;
-	public HttpURLConnection conn = null;
+	 public static String number;
+	 public static String valid;
+	 public static String localformat;
+	 public static String intformat;
+	 public static String carrier;
+	 public static String countrycode;
+	 public static String location;
+	 public static String linetype;
 	
 	
-	public JSONObject sendGet(String phonenum) throws Exception{
-		
+	public void sendGet(String phonenum) throws Exception{
 		Config conf = new Config();
 		String ak = conf.getVirusTotalAPIKey();
-		
 	    String url1 = "http://apilayer.net/api/validate?access_key=";
 	    String url2 = "&number=" + phonenum;
-		//String url2 = "&number=14158586273";
 		String url = url1 + ak + url2;
 		
-		//Scanner reader = new Scanner(System.in);
-		//System.out.println("Enter a phone number: ");
-		//int num = reader.nextInt();
-		//reader.close();
-		
-		//url = url + num;
-		try {
 		URL obj = new URL(url);
 		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 		conn.setRequestMethod("GET");
@@ -47,44 +41,35 @@ public class Test_RestAPIClient {
 			response.append(inputLine);
 		}
 		in.close();
-
-		//print result
-		//System.out.println(response.toString());
 		
 		JSONParser jsonParser = new JSONParser();
 		JSONObject jsonObject = (JSONObject) jsonParser.parse(response.toString());
-		
-		//String valid = (String) jsonObject.get("valid");
-		number = (String) jsonObject.get("number");
-		//String number = (String) jsonObject.get("number");
-		String countrycode = (String) jsonObject.get("country_code");
-	    carrier  = (String) jsonObject.get("carrier");
-		//System.out.println("valid : " + valid);
-		//System.out.println("number : " + number);
-		//System.out.println("countrycode : " + countrycode);
-		//System.out.println("carrier : " + carrier);
-		 // get an array from the JSON object
-		//JSONArray lang= (JSONArray) jsonObject.get("languages");
-		
-		return jsonObject;
-		}finally {
-	    	conn.disconnect();
-	    }
+
+		 number = (String) jsonObject.get("number");
+		 valid = (String) jsonObject.get("valid");
+		 localformat = (String) jsonObject.get("local_format");
+		 intformat = (String) jsonObject.get("international_format");
+		 carrier  = (String) jsonObject.get("carrier");
+		 countrycode = (String) jsonObject.get("country_code");
+		 location = (String) jsonObject.get("location");
+		 linetype = (String) jsonObject.get("line_type");
+	
+	    conn.disconnect();
 	   
 	}
 	
-	
-	
-	
-	
 	public static void main(String[] args) {
-			JSONObject resp;
 			Test_RestAPIClient client = new Test_RestAPIClient();
 			try{
-			     resp = client.sendGet("14158586273");
-				//resp = client.jsonObject;
-			    carrier = (String) resp.get("carrier");
-				System.out.println("carrier : " + carrier);
+			     client.sendGet("14158586273");
+			     System.out.println(client.valid);
+			     System.out.println(client.number);
+			     System.out.println(client.localformat);
+			     System.out.println(client.intformat);
+			     System.out.println(client.carrier);
+			     System.out.println(client.countrycode);
+			     System.out.println(client.location);
+			     System.out.println(client.linetype);
 			
 		} catch (Exception e) {
 			System.out.println(e + " was thrown.");
